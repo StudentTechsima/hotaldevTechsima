d33dc6f7b6df500ced5e91d1d65fa2d668f84079
StudentTechsima <techsimatraining@gmail.com>
2024-12-28 15:17:40 +0530

    first commit

---
+++ contact_us.php
@@ -0,0 +1,58 @@
+<?php  include "layout/header.php"?>
+  <section class="page">
+    <div class="container_contact_us">
+      <div class="contact">
+
+        <form>
+            <h1>CONTACT US FORM</h1>
+            <input class="contactinput" type="text" id="name" placeholder="YOUR NAME" name="name" required >
+            <input class="contactinput" type="email" id="email" placeholder=" YOUR EMAIL" name="email" required >
+            <input class="contactinput" type="text" id="subject" placeholder="SUBJECT" name="subject" required >
+            <textarea class="textarea1" placeholder="YOUR MESSAGE (OPTION)." rows="8" required></textarea>  
+            <input class="submit_btn" type="Submit" value="Submit" class="submit_btn">
+        </form>
+      </div>
+      <div class="get">
+        
+          <h1>GET IN TOUCH</h1>
+          <div class="gfield">
+          <h2>ADDRESS:</h2>
+          <P>In front of Rajarshi Dashrath Rajkiya Medical College, Darshan Nagar, Ayodhya, Uttar Pradesh- 224135</P>
+        </div>
+        
+        
+          <div class="gfield">
+            <h2>EMAIL:</h2>
+            <P>info@gmail.com</P>
+          </div>
+          
+          <div class="gfield">
+            <h2>WEB:</h2>
+            <P>www.techsimahotel.in</P>
+          </div>
+          <div class="gfield">
+            <h2>PHONE:</h2>
+            <P>7800 050 976</P>
+          </div>
+          <div class="social">
+            <a href="#" class="contact-social">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#" class="contact-social">
+            <i class="fa-brands fa-instagram"></i>
+          </a >
+          <a href="#" class="contact-social">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="#" class="contact-social">
+            <i class="fa-brands fa-youtube"></i>
+          </a>
+          <a href="#" class="contact-social">
+            <i class="fa-brands fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+  </section>
+  <?php include "layout/footer.php"?>
+  
